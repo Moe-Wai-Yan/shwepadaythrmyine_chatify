a4ea467e3becb9de13c8dd7fd7f7ef1de54d19c5
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-09-29 14:33:01 +0630

    fix the migration

--- src/database/migrations/2022_01_10_99999_add_active_status_to_users.php
+++ src/database/migrations/2022_01_10_99999_add_active_status_to_users.php
@@ -13,9 +13,9 @@ class AddActiveStatusToUsers extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('ec_users', function (Blueprint $table) {
             // if not exist, add the new column
-            if (!Schema::hasColumn('users', 'active_status')) {
+            if (!Schema::hasColumn('ec_users', 'active_status')) {
                 $table->boolean('active_status')->default(0);
             }
         });
@@ -28,7 +28,7 @@ class AddActiveStatusToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('ec_users', function (Blueprint $table) {
             $table->dropColumn('active_status');
         });
     }
